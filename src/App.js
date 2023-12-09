a3676644797be7a209e039b687adfdf7985de0d1
hphamp <conghuypham.work@gmail.com>
2023-12-09 14:41:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [orderId, setOrderId] = useState('');
+
+  const handleDelivery = () => {
+    // Xử lý logic khi đơn hàng đang giao
+    console.log(`Đang giao đơn hàng ${orderId}`);
+  };
+
+  const handleReceived = () => {
+    // Xử lý logic khi đơn hàng đã nhận hàng
+    console.log(`Đã nhận hàng ${orderId}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Shipper App</h1>
+        <div>
+          <label>Nhập ID đơn hàng:</label>
+          <input
+            type="text"
+            value={orderId}
+            onChange={(e) => setOrderId(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={handleDelivery}>Đã lấy hàng</button>
+          <button onClick={handleReceived}>Đã giao hàng</button>
+        </div>
       </header>
+      <img
+        src="http://shopiec.shop/static/media/logoRed.b15e7a509ea97bb9a56d.png"
+        alt="Shopiec Logo"
+        style={{ width: '100%', height: 'auto' }}
+      />
     </div>
   );
 }
